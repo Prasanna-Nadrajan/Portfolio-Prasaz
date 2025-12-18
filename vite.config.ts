bfa137fc48b5786520daf20c97d03a5704e94a17
Prasanna Nadrajan <prasannanadrajan@gmail.com>
2025-12-18 09:53:17 +0530

    seo_final

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,9 @@
-// vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import Sitemap from 'vite-plugin-sitemap'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    react(),
-    Sitemap({
-      hostname: 'https://prasaz.vercel.app',
-      dynamicRoutes: [
-        '/',
-        '/portfolio',
-        '/experience',
-        '/blog',
-        '/platforms',
-        '/resume',
-        '/contact',
-        '/github-wrapped',
-        '/leetcode-wrapped'
-      ]
-    })
+    react()
   ],
 })
